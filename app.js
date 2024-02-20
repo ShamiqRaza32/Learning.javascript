dfea842edca074120d1bbd474cf8ac58ed7594f5
Shamiq Raza <155806245+ShamiqRaza32@users.noreply.github.com>
2024-02-20 12:45:46 +0500

    Add files via upload
    first class practice in javascript

---
+++ app.js
@@ -0,0 +1,18 @@
+let myName ="shamiq raza";
+
+console.log(myName);
+console.log(myName);
+console.log(myName);
+console.log(myName);
+console.log(myName);
+console.log(myName);
+console.log(myName);
+
+// let age =50;
+
+// let age =50;0
+
+console.log(5+4);
+
+console.log("5+4");
+
